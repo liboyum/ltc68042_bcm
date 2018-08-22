6e2732c2a201fb3135b254ac72324f48cd74a643
liboyum <34636303+liboyum@users.noreply.github.com>
2018-08-22 11:42:30 -0400

    Update ltc68042_bcm.h

--- ltc68042_bcm.h
+++ ltc68042_bcm.h
@@ -163,8 +163,8 @@ void wakeup_sleep();
 
 uint16_t pec15_calc(uint8_t len, uint8_t *data);
 
-void spi_write_array( uint8_t len, uint8_t data[]);
+void spi_write_array( uint8_t len, uint8_t *data);
 
-void spi_write_read(uint8_t tx_Data[], uint8_t tx_len, uint8_t rx_data[], uint8_t rx_len);
+void spi_write_read(uint8_t tx_Data[], uint8_t tx_len, uint8_t *rx_data, uint8_t rx_len);
 
 #endif
