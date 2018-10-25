b37d704b8cb654489f9e104664b6e955312e8bdf
liboyum <34636303+liboyum@users.noreply.github.com>
2018-10-25 15:11:07 -0400

    Update main.c

--- main.c
+++ main.c
@@ -4,10 +4,9 @@
 #include <bcm2835.h>
 #include "ltc68042_bcm.h"
 
-#define TOTAL_IC 1
-
+const int TOTAL_IC = 1;
 uint16_t cell_codes[TOTAL_IC][12];
-uint16_t aux_codes[TOTAL_IC][6];
+uint8_t tx_cfg[TOTAL_IC][6];
 int rdError = 0;
 
 int main(int argc, char **argv)
@@ -24,8 +23,18 @@ int main(int argc, char **argv)
       return 1;
     }
     LTC6804_initialize();
+    for(int i = 0; i<TOTAL_IC;i++)
+    {
+      tx_cfg[i][0] = 0xFE;
+      tx_cfg[i][1] = 0x00 ; 
+      tx_cfg[i][2] = 0x00 ;
+      tx_cfg[i][3] = 0x00 ; 
+      tx_cfg[i][4] = 0x00 ;
+      tx_cfg[i][5] = 0x00 ;
+    }
   while(1)
   {	
+    LTC6804_wrcfg(TOTAL_IC,tx_cfg);
     LTC6804_adcv();
     rdError = LTC6804_rdcv(0, TOTAL_IC, cell_codes);
     if(rdError == -1){
@@ -35,7 +44,7 @@ int main(int argc, char **argv)
     else{
 	for(int i=0; i<12; i++)
 	{
-		printf("The voltage is %f\n", cell_codes[TOTAL_IC][i]*0.0001);
+		printf("The voltage is %f\n", cell_codes[TOTAL_IC][i]);
 	}
     }
     bcm2835_delay(500);
