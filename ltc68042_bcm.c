aabe046e775b819f8f427447fa2e5d93a0b8f355
liboyum <34636303+liboyum@users.noreply.github.com>
2018-10-25 16:15:01 -0400

    Update ltc68042_bcm.c

--- ltc68042_bcm.c
+++ ltc68042_bcm.c
@@ -14,7 +14,7 @@ void LTC6804_initialize()
 {
   bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
   bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_4096); // The default
+  bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256);   // The default
   bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
   bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
   set_adc(MD_NORMAL,DCP_DISABLED,CELL_CH_ALL,AUX_CH_ALL);
