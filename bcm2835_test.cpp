e2a406a82c8e7919bebb2ba167b6d12026787739
liboyum <34636303+liboyum@users.noreply.github.com>
2018-11-05 14:58:14 -0500

    Update bcm2835_test.cpp

--- bcm2835_test.cpp
+++ bcm2835_test.cpp
@@ -34,26 +34,26 @@ int main()
 	uint8_t rdcmd1 = 0x04;
 	uint8_t pec2 = 0x77;
 	uint8_t pec3 = 0xD6;
-
-	bcm2835_spi_transfer(rdcmd0);
-	bcm2835_spi_transfer(rdcmd1);
-	bcm2835_spi_transfer(pec2);
-	bcm2835_spi_transfer(pec3);
+	uint16_t cell_codes[4];
+	cell_codes[0] = bcm2835_spi_transfer(rdcmd0);
+	cell_codes[1] = bcm2835_spi_transfer(rdcmd1);
+	cell_codes[2] = bcm2835_spi_transfer(pec2);
+	cell_codes[3] = bcm2835_spi_transfer(pec3);
 	
-	uint8_t cell_data[8];
-	cell_data[0] = bcm2835_spi_transfer(0x00);
-	cell_data[1] = bcm2835_spi_transfer(0x00);
-	cell_data[2] = bcm2835_spi_transfer(0x00);
-	cell_data[3] = bcm2835_spi_transfer(0x00);
-	cell_data[4] = bcm2835_spi_transfer(0x00);
-	cell_data[5] = bcm2835_spi_transfer(0x00);
-	cell_data[6] = bcm2835_spi_transfer(0x00);
-	cell_data[7] = bcm2835_spi_transfer(0x00);
+// 	uint8_t cell_data[8];
+// 	cell_data[0] = bcm2835_spi_transfer(0x00);
+// 	cell_data[1] = bcm2835_spi_transfer(0x00);
+// 	cell_data[2] = bcm2835_spi_transfer(0x00);
+// 	cell_data[3] = bcm2835_spi_transfer(0x00);
+// 	cell_data[4] = bcm2835_spi_transfer(0x00);
+// 	cell_data[5] = bcm2835_spi_transfer(0x00);
+// 	cell_data[6] = bcm2835_spi_transfer(0x00);
+// 	cell_data[7] = bcm2835_spi_transfer(0x00);
 	
-	uint16_t cell_codes[3];
-	cell_codes[0] = cell_data[0] + (cell_data[1]<<8);
-	cell_codes[1] = cell_data[2] + (cell_data[3]<<8);
-	cell_codes[2] = cell_data[4] + (cell_data[5]<<8);
+// 	uint16_t cell_codes[3];
+// 	cell_codes[0] = cell_data[0] + (cell_data[1]<<8);
+// 	cell_codes[1] = cell_data[2] + (cell_data[3]<<8);
+// 	cell_codes[2] = cell_data[4] + (cell_data[5]<<8);
 	printf("%d\n", cell_codes[0]);
 	printf("%d\n", cell_codes[1]);
 	printf("%d\n", cell_codes[2]);
