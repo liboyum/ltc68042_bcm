89f17eb85b8b9f58e1f29b1c3ba322b2054352db
liboyum <34636303+liboyum@users.noreply.github.com>
2018-11-05 11:30:28 -0500

    Update bcm2835_test.cpp

--- bcm2835_test.cpp
+++ bcm2835_test.cpp
@@ -14,33 +14,47 @@ int main()
 		printf("bcm2835_spi_begin failed. Are you running as root?\n");
 		return 1;
 	}
-    bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
-    bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-    bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
-    bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
-    bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
+        bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+    	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
+    	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
+    	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
+    	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 	//ADCV command for cell 1&7
-	uint8_t comm0 = 0x03;
-	uint8_t comm1 = 0x61;
-	uint8_t pec0 = 0x7f;
-	uint8_t pec1 = 0x5e;
-	bcm2835_spi_transfer(comm0);
-	bcm2835_spi_transfer(comm1);
-	bcm2835_spi_transfer(pec0);
-	bcm2835_spi_transfer(pec1);
+	// uint8_t comm0 = 0x03;
+	// uint8_t comm1 = 0x61;
+	// uint8_t pec0 = 0x7f;
+	// uint8_t pec1 = 0x5e;
+	// bcm2835_spi_transfer(comm0);
+	// bcm2835_spi_transfer(comm1);
+	// bcm2835_spi_transfer(pec0);
+	// bcm2835_spi_transfer(pec1);
+	char cmd[] = {0x03,0x61,0x7f,0x5e};
+	bcm2835_spi_transfern(cmd,sizeof(cmd));
 	//RDCV command for cell 1&7
 	uint8_t rdcmd0 = 0x00;
 	uint8_t rdcmd1 = 0x04;
 	uint8_t pec2 = 0x07;
 	uint8_t pec3 = 0xc2;
-	uint8_t cell_codes[2];
-	cell_codes[0] = bcm2835_spi_transfer(rdcmd0);
-	cell_codes[1] = bcm2835_spi_transfer(rdcmd1);
-	bcm2835_spi_transfer(pec2);
-	bcm2835_spi_transfer(pec3);
+	uint16_t cell_data[4];
+	cell_data[0] = bcm2835_spi_transfer(rdcmd0);
+	cell_data[1] = bcm2835_spi_transfer(rdcmd1);
+	cell_data[2] = bcm2835_spi_transfer(pec2);
+	cell_data[3] = bcm2835_spi_transfer(pec3);
 	
-	printf("%f\n", cell_data[0]);
-	printf("%f\n", cell_data[1]);
+	// uint8_t cell_data[8];
+	// cell_data[0] = bcm2835_spi_transfer(0xFF);
+	// cell_data[1] = bcm2835_spi_transfer(0xFF);
+	// cell_data[2] = bcm2835_spi_transfer(0xFF);
+	// cell_data[3] = bcm2835_spi_transfer(0xFF);
+	// cell_data[4] = bcm2835_spi_transfer(0xFF);
+	// cell_data[5] = bcm2835_spi_transfer(0xFF);
+	// cell_data[6] = bcm2835_spi_transfer(0xFF);
+	// cell_data[7] = bcm2835_spi_transfer(0xFF);
+	
+	printf("%d\n", cell_data[0]);
+	printf("%d\n", cell_data[1]);
+	printf("%d\n", cell_data[2]);
+	printf("%d\n", cell_data[3]);
 	
 	bcm2835_spi_end();
 	bcm2835_close();
