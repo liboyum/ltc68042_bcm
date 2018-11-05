2609f4bc7950bcdd007711b697b9c559814220a1
liboyum <34636303+liboyum@users.noreply.github.com>
2018-11-05 12:16:02 -0500

    Update bcm2835_test.cpp

--- bcm2835_test.cpp
+++ bcm2835_test.cpp
@@ -14,9 +14,9 @@ int main()
 		printf("bcm2835_spi_begin failed. Are you running as root?\n");
 		return 1;
 	}
-        bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
+	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);      // The default
     	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);                   // The default
-    	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_65536); // The default
+    	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_256);   // The default
     	bcm2835_spi_chipSelect(BCM2835_SPI_CS0);                      // The default
     	bcm2835_spi_setChipSelectPolarity(BCM2835_SPI_CS0, LOW);      // the default
 	//ADCV command for cell 1&7
@@ -42,16 +42,17 @@ int main()
 	bcm2835_spi_write(pec3);
 	
 	uint8_t cell_data[8];
-	cell_data[0] = bcm2835_spi_transfer(0xFF);
-	cell_data[1] = bcm2835_spi_transfer(0xFF);
-	cell_data[2] = bcm2835_spi_transfer(0xFF);
-	cell_data[3] = bcm2835_spi_transfer(0xFF);
-	cell_data[4] = bcm2835_spi_transfer(0xFF);
-	cell_data[5] = bcm2835_spi_transfer(0xFF);
-	cell_data[6] = bcm2835_spi_transfer(0xFF);
-	cell_data[7] = bcm2835_spi_transfer(0xFF);
+	cell_data[0] = bcm2835_spi_transfer(0x00);
+	cell_data[1] = bcm2835_spi_transfer(0x00);
+	cell_data[2] = bcm2835_spi_transfer(0x00);
+	cell_data[3] = bcm2835_spi_transfer(0x00);
+	cell_data[4] = bcm2835_spi_transfer(0x00);
+	cell_data[5] = bcm2835_spi_transfer(0x00);
+	cell_data[6] = bcm2835_spi_transfer(0x00);
+	cell_data[7] = bcm2835_spi_transfer(0x00);
+	
 	uint16_t cell_codes[3];
-	cell_codes[0] = (cell_data[0]>>8) + (cell_data[1]);
+	cell_codes[0] = cell_data[0] + (cell_data[1]<<8);
 	cell_codes[1] = cell_data[2] + (cell_data[3]<<8);
 	cell_codes[2] = cell_data[4] + (cell_data[5]<<8);
 	printf("%d\n", cell_codes[0]);
